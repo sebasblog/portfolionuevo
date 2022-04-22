86d3917b708d39fee09bd50618448c4880e7dd5d
Sebastian Pernia <sebaspernia@gmail.com>
2022-04-22 19:42:38 +0200

    cambios 22-04

--- index.js
+++ index.js
@@ -27,3 +27,18 @@ $(document).ready(function(){
 });
 
 
+
+function irArriba(pxPantalla) {
+    window.addEventListener('scroll', () =>{
+        var scroll = document.documentElement.scrollTop;
+        var botonArriba = document.getElementById('botonArriba');
+
+        if(scroll > pxPantalla){
+            botonArriba.style.right = 20 + "px";
+        } else {
+            botonArriba.style.right = -120 + "px";
+        }
+    })
+}
+
+irArriba(300);
